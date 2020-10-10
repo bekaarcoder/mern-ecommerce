aacd05a639b890cce91681073d5f40e46b46781a
Shashank <s_shashank@outlook.com>
2020-10-11 02:38:57 +0530

    added custom error handling

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connect_db from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { not_found, error_handler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -10,6 +11,10 @@ connect_db();
 const app = express();
 
 app.use("/api/products", productRoutes);
+
+app.use(not_found);
+
+app.use(error_handler);
 
 const port = process.env.PORT || 5000;
 
