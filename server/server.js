e88e7deb722693b15fb47061cca1108496d53467
Shashank <s_shashank@outlook.com>
2020-10-15 22:38:15 +0530

    user authentication endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connect_db from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { not_found, error_handler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -10,7 +11,10 @@ connect_db();
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(not_found);
 
