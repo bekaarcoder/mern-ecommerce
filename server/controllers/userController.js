fde821acfb09a9fd746ac98261465b834d3328c4
Shashank <s_shashank@outlook.com>
2020-10-17 13:29:16 +0530

    added user registration api and password encryption

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -28,7 +28,52 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("success");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
-export { authUser, getUserProfile };
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const newUser = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: generateToken(newUser._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
